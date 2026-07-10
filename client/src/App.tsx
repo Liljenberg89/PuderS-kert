15aae23d77add9d80b665905d85caea1ab60fa88
Filip Liljenberg <filipliljenberg9@gmail.com>
2026-07-10 19:17:52 +0200

    Added function to calculate current week

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,17 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [week, setWeek] = useState<number>(19);
+  function getISOWeek(date: Date): number {
+    const d = new Date(
+      Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()),
+    );
+    const dayNum = d.getUTCDay() || 7;
+    d.setUTCDate(d.getUTCDate() + 4 - dayNum);
+    const yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
+    return Math.ceil(((d.getTime() - yearStart.getTime()) / 86400000 + 1) / 7);
+  }
+
+  const [week, setWeek] = useState<number>(getISOWeek(new Date()));
   return (
     <div className="container">
       <div className="sectionOne">
