cfa3d81bf1e30ad4b32665ea84b9854013fc5478
Filip Liljenberg <filipliljenberg9@gmail.com>
2026-07-10 18:19:33 +0200

    small start with simple design

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,20 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <></>;
+  const [week, setWeek] = useState<number>(19);
+  return (
+    <div className="container">
+      <div className="sectionOne">
+        <div className="intro">
+          <h1>PuderSäkert</h1>
+          <h4>Av puderåkare, för puderåkare!</h4>
+          <div className="week-input">
+            <h2>Vecka {week}</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
